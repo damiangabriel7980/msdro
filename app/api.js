3a5dfe81940e417a315772c49a7b3ec0389c49e5
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2014-10-20 10:08:15 +0300

    Removed exports that could allow user to alter database

--- app/api.js
+++ app/api.js
@@ -15,9 +15,7 @@ module.exports = function(app, router) {
 
                 res.json(cont);
             });
-        })
-
-
+        });
 
     router.route('/content/:content_id')
 
@@ -45,28 +43,6 @@ module.exports = function(app, router) {
 
     router.route('/products')
 
-        .post(function(req, res) {
-
-            var product = new Products();
-
-            product.version_prod = req.body.version_prod;
-            product.description = req.body.description;
-            product.enableP = req.body.enableP;
-            product.file_path = req.body.file_path;
-            product.image_path = req.body.image_path;
-            product.last_updated = new Date();
-            product.nameP = req.body.nameP;
-
-            product.save(function(err){
-                if(err) {
-                    res.send(err);
-                }
-
-                res.json(product);
-            });
-
-        })
-
         .get(function(req, res) {
             Products.find(function(err, cont) {
                 if(err) {
@@ -87,69 +63,9 @@ module.exports = function(app, router) {
 
                 res.json(cont);
             })
-        })
-
-        .put(function(req, res) {
-            Products.findById(req.params.products_id, function(err, cont) {
-                if(err) {
-                    res.send(err);
-                }
-
-
-                cont.version_prod = req.body.version_prod;
-                cont.description = req.body.description;
-                cont.enableP = req.body.enableP;
-                cont.file_path = req.body.file_path;
-                cont.image_path = req.body.image_path;
-                cont.last_updated = new Date();
-                cont.nameP = req.body.nameP;
-
-                cont.save(function(err){
-                    if(err) {
-                        res.send(err);
-                    }
-
-                    res.json({ message: "Product updated!"});
-                })
-            });
-
-        })
-
-        .delete(function(req, res) {
-            Products.remove({
-                _id: req.params.products_id
-            }, function(err, cont) {
-                if(err) {
-                    res.send(err);
-                }
-
-                res.json({ message: 'Succesfully deleted'});
-            })
         });
 
     router.route('/therapeutic_areas')
-
-        .post(function(req, res) {
-
-            var therapeutic_area = new Therapeutic_Area();
-
-            therapeutic_area.version_ther = req.body.version_ther;
-            therapeutic_area.has_children = req.body.has_children;
-            therapeutic_area.last_updated = new Date();
-            therapeutic_area.enable = req.body.enable;
-            therapeutic_area.parent_id = req.body.parent_id;
-
-            therapeutic_area.name = req.body.name;
-
-            therapeutic_area.save(function(err){
-                if(err) {
-                    res.send(err);
-                }
-
-                res.json(therapeutic_area);
-            });
-
-        })
 
         .get(function(req, res) {
             Therapeutic_Area.find(function(err, cont) {
@@ -161,8 +77,6 @@ module.exports = function(app, router) {
             });
         });
 
-
-
     router.route('/therapeutic_areas/:therapeutic_areas_id')
 
         .get(function(req, res) {
@@ -172,43 +86,6 @@ module.exports = function(app, router) {
                 }
 
                 res.json(cont);
-            })
-        })
-
-        .put(function(req, res) {
-            Therapeutic_Area.findById(req.params._id, function(err, cont) {
-                if(err) {
-                    res.send(err);
-                }
-
-
-                cont.version_ther = req.body.version_ther;
-                cont.has_children = req.body.has_children;
-                cont.last_updated = new Date();
-                cont.enable = req.body.enable;
-                cont.parent_id = req.body.parent_id;
-                cont.name = req.body.name;
-
-                cont.save(function(err){
-                    if(err) {
-                        res.send(err);
-                    }
-
-                    res.json({ message: "Area updated!"});
-                })
-            });
-
-        })
-
-        .delete(function(req, res) {
-            Therapeutic_Area.remove({
-                _id: req.params.therapeutic_areas_id
-            }, function(err, cont) {
-                if(err) {
-                    res.send(err);
-                }
-
-                res.json({ message: 'Succesfully deleted'});
             })
         });
 
