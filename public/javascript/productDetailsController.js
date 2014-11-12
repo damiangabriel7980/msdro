742d89ca884fb79e64d819c6fc61372e8e19e716
miricaandrei23 <mirica.andrei23@yahoo.com>
2014-11-12 11:28:13 +0200

    Revert to Mongo-2

---
+++ public/javascript/productDetailsController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by miricaandrei23 on 03.11.2014.
+ */
+cloudAdminControllers.controller('productDetailsController', ['$scope','$rootScope' ,'ProductService','$stateParams','$sce', function($scope,$rootScope,ProductService,$stateParams,$sce) {
+     ProductService.getSingle.query({id:$stateParams.id}).$promise.then(function(result){
+         $scope.selectedProduct = result;
+         $scope.ProductDetailsHTML = $sce.trustAsHtml(result.description);
+     });
+
+    $scope.amazon = $rootScope.pathAmazonDev;
+}]);
---
+++ public/javascript/testeController.js
@@ -0,0 +1,26 @@
+/**
+ * Created by miricaandrei23 on 05.11.2014.
+ */
+cloudAdminControllers.controller('testeController', ['$scope','$rootScope' ,'testeService','$stateParams','$sce', function($scope,$rootScope,testeService,$stateParams,$sce){
+    var date = new Date();
+    testeService.getAll.query().$promise.then(function(result){
+        $scope.teste = result;
+        //$scope.testeFiltered = [];
+        //for (var i = 0; i < $scope.teste.length; i++) {
+        //    if (new Date($scope.teste[i].expiry_date).getMonth() + 1 >= date.getMonth() + 1 && (new Date($scope.teste[i].expiry_date).getDay() >= date.getDay()))
+        //        $scope.testeFiltered.push($scope.teste[i]);
+        //}
+    });
+    //$scope.questions = testeService.getByTest.query({id:$stateParams.id});
+    $scope.amazon = $rootScope.pathAmazonDev;
+}])
+    .filter('htmlToPlaintext', function() {
+        return function(text) {
+            return String(text).replace(/<[^>]+>/gm, '').replace(/&nbsp;/g,' ');
+        }
+    })
+.filter("asDate", function () {
+    return function (input) {
+        return new Date(input);
+    }
+});
