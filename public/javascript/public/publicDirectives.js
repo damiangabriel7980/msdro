d19fd26400adcaffd2b7a808bb59275c199ec5b0
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2014-12-15 16:19:31 +0200

    Home styles

--- public/javascript/public/publicDirectives.js
+++ public/javascript/public/publicDirectives.js
@@ -1,20 +1,25 @@
 publicApp.directive('resizable', function($window) {
     return {
         restrict: 'A',
-        link: function ($scope, $element) {
+        link: function ($scope, $element, attrs) {
 
-            $scope.initializeWindowSize = function () {
-                $scope.elementWidth = angular.element($element)[0].offsetWidth;
-                $scope.elementHeight = angular.element($element)[0].offsetHeight;
+            var ratio = attrs.ratio?attrs.ratio:1;
+            var elem = angular.element($element);
+
+            var initializeElementSize = function () {
+                var elemWidth = elem[0].offsetWidth;
+                var elemHeight = elem[0].offsetHeight;
+
+                elem.css('height', elemWidth/ratio+'px');
             };
 
             angular.element($window).bind('resize', function () {
-                $scope.initializeWindowSize();
+                initializeElementSize();
                 $scope.$apply();
             });
 
             // Initiate the resize function default values
-            $scope.initializeWindowSize();
+            initializeElementSize();
         }
     }
 });
