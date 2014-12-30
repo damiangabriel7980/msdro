b3d9a5d0cf8f9baf6397327a03fe1b9a055dec45
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2014-12-30 13:01:26 +0200

    Responsive carousel

---
+++ public/javascript/medic/customDirectives.js
@@ -0,0 +1,38 @@
+/**
+ * Created by andrei on 03.12.2014.
+ */
+//similar use as ng-src. Forces server to request image, instead of loading it from cache
+cloudAdminApp.directive('noCacheSrc', function($window) {
+    return {
+        priority: 99,
+        link: function(scope, element, attrs) {
+            attrs.$observe('noCacheSrc', function(noCacheSrc) {
+                noCacheSrc += '?' + (new Date()).getTime();
+                attrs.$set('src', noCacheSrc);
+            });
+        }
+    }
+}).directive('carouselResizable', function($window) {
+    return {
+        restrict: 'A',
+        link: function ($scope, $element) {
+
+            $scope.initializeWindowSize = function () {
+                $scope.elementWidth = angular.element($element)[0].offsetWidth;
+                //$scope.elementHeight = angular.element($element)[0].offsetHeight;
+
+                var carouselH = $scope.elementWidth / 4;
+
+                $scope.carouselResponsiveStyle = 'height:' + carouselH + 'px;';
+            };
+
+            angular.element($window).bind('resize', function () {
+                $scope.initializeWindowSize();
+                $scope.$apply();
+            });
+
+            // Initiate the resize function default values
+            $scope.initializeWindowSize();
+        }
+    }
+});
