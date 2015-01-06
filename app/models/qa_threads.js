ca49dffc7b5d9feaaf395a0c2ae00f9970f20761
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-01-06 11:51:49 +0200

    Changes in thread and messages models

--- app/models/qa_threads.js
+++ app/models/qa_threads.js
@@ -6,12 +6,14 @@ var Schema			= mongoose.Schema;
 
 var threadsSchema		= new Schema({
     owner: {type: Schema.Types.ObjectId,ref: 'User'},
+    ownerDisplay: String,
     question: String,
     date_recorded: Date,
-    medics: [{type: Schema.Types.ObjectId,ref: 'User'}],
-    topics:[{type: Schema.Types.ObjectId,ref: 'topics'}],
-    locked: {type: Schema.Types.ObjectId,ref: 'User'},
-    messages:[{type: Schema.Types.ObjectId,ref: 'qa_messages'}]
+    medics: [{type: Schema.Types.ObjectId,ref: 'qa_answerGivers'}],
+    topics:[{type: Schema.Types.ObjectId,ref: 'qa_topics'}],
+    locked: {type: Schema.Types.ObjectId,ref: 'qa_answerGivers'},
+    messages:[{type: Schema.Types.ObjectId,ref: 'qa_messages'}],
+    answered: Boolean
 });
 
 module.exports = mongoose.model('qa_threads', threadsSchema,'qa_threads');
