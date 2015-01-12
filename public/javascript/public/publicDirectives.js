c3d488b998a907dc481efda707d8b48c2be908f8
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-01-12 13:56:59 +0200

    Carousel simplifications

--- public/javascript/public/publicDirectives.js
+++ public/javascript/public/publicDirectives.js
@@ -30,20 +30,13 @@ publicApp.directive('carouselResizable', function($window) {
 
             $scope.initializeWindowSize = function () {
                 $scope.elementWidth = angular.element($element)[0].offsetWidth;
-                $scope.elementHeight = angular.element($element)[0].offsetHeight;
+                //$scope.elementHeight = angular.element($element)[0].offsetHeight;
 
-                var carouselH = $scope.elementWidth / 3;
-                var offset_Y = carouselH / 10;
+                var carouselH = Math.round($scope.elementWidth / 3);
 
                 $scope.carouselStyle = 'height:' + carouselH + 'px;' +
                     'width:' + ($scope.elementWidth) + 'px;';
 
-                $scope.fixedBoxStyle = 'top:' + offset_Y + 'px;' +
-                    'height:' + (carouselH - (2 * offset_Y)) + 'px;' +
-                    'width:' + ($scope.elementWidth / 2) + 'px;';
-
-                $scope.cNavStyle = 'bottom:' + offset_Y + 'px;' +
-                    'width:' + ($scope.elementWidth / 2 - 40) + 'px;';
             };
 
             angular.element($window).bind('resize', function () {
