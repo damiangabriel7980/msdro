6d6c6f5b1c160b39dc51e2e10bfdbfc25b07171c
miricaandrei23 <mirica.andrei23@yahoo.com>
2015-01-19 10:25:02 +0200

    Solved search results issue( null values were displayed)

--- public/javascript/medic/HomeSearchController.js
+++ public/javascript/medic/HomeSearchController.js
@@ -10,11 +10,44 @@ cloudAdminControllers.controller('HomeSearchController', ['$scope', '$rootScope'
     };
     $scope.amazon = $rootScope.pathAmazonDev;
     HomeService.getSearchResults.query({data:$stateParams.data}).$promise.then(function(response){
-       $scope.products = response.products;
-        $scope.quizes= response.quizes;
-        $scope.multimedia=response.multimedia;
-        $scope.articles=response.articles;
-        $scope.calendarEv = response['calendar-events'];
+       $scope.productsFirstSet = response.products;
+        $scope.products=[];
+        $scope.quizes=[];
+        $scope.multimedia=[];
+        $scope.articles=[];
+        $scope.calendarEv=[];
+        for(var i=0;i<$scope.productsFirstSet.length;i++)
+        {
+            if($scope.productsFirstSet[i]!=null)
+                $scope.products.push($scope.productsFirstSet[i]);
+        }
+
+        $scope.quizesFirstSet= response.quizes;
+        for(var i=0;i<$scope.quizesFirstSet.length;i++)
+        {
+            if($scope.quizesFirstSet[i]!=null)
+                $scope.quizes.push($scope.quizesFirstSet[i]);
+        }
+
+        $scope.multimediaFirstSet=response.multimedia;
+        for(var i=0;i<$scope.multimediaFirstSet.length;i++)
+        {
+            if($scope.multimediaFirstSet[i]!=null)
+                $scope.multimedia.push($scope.multimediaFirstSet[i]);
+        }
+
+        $scope.articlesFirstSet=response.articles;
+        for(var i=0;i<$scope.articlesFirstSet.length;i++)
+        {
+            if($scope.articlesFirstSet[i]!=null)
+                $scope.articles.push($scope.articlesFirstSet[i]);
+        }
+        $scope.calendarEvFirstSet = response['calendar-events'];
+        for(var i=0;i<$scope.calendarEvFirstSet.length;i++)
+        {
+            if($scope.calendarEvFirstSet[i]!=null)
+                $scope.calendarEv.push($scope.calendarEvFirstSet[i]);
+        }
     });
     $scope.sref=function(artType){
         if(artType < 3)
