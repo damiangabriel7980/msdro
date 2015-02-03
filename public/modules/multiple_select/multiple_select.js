0965e89e9d9d5de44905f9ffcc37d4e0e0b872c7
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-02-03 18:05:06 +0200

    Wrapped multiple select directive code in self invoking function

--- public/modules/multiple_select/multiple_select.js
+++ public/modules/multiple_select/multiple_select.js
@@ -1,89 +1,91 @@
-var scripts = document.getElementsByTagName("script");
-var currentScriptPath = scripts[scripts.length-1].src;
+(function() {
+    var scripts = document.getElementsByTagName("script");
+    var currentScriptPath = scripts[scripts.length-1].src;
 
-angular.module('myMultipleSelect', []).directive('myMultipleSelect', function() {
-    return {
-        restrict: 'E',
-        templateUrl: currentScriptPath.replace('multiple_select.js', 'multiple_select_template.html'),
-        replace: true,
-        scope: {
-            allObjects: '=',
-            selectedObjects: '='
-        },
-        link: function(scope, element, attrs) {
-            
-            var initialized = false;
-            var allObjects = null;
+    angular.module('myMultipleSelect', []).directive('myMultipleSelect', function() {
+        return {
+            restrict: 'E',
+            templateUrl: currentScriptPath.replace('multiple_select.js', 'multiple_select_template.html'),
+            replace: true,
+            scope: {
+                allObjects: '=',
+                selectedObjects: '='
+            },
+            link: function(scope, element, attrs) {
 
-            var trackBy = attrs.trackBy;
-            scope.displayBy = attrs.displayBy;
-            var splitBy = attrs.splitBy;
-            scope.split = splitBy?true:false;
+                var initialized = false;
+                var allObjects = null;
 
-            scope.$watch('allObjects', function () {
-                initialize();
-            });
+                var trackBy = attrs.trackBy;
+                scope.displayBy = attrs.displayBy;
+                var splitBy = attrs.splitBy;
+                scope.split = splitBy?true:false;
 
-            scope.$watch('selectedObjects', function () {
-                initialize();
-            });
+                scope.$watch('allObjects', function () {
+                    initialize();
+                });
 
-            var getValueFromKeyChain = function (obj, chain) {
-                for(var i=0; i<chain.length; i++){
-                    obj=obj[chain[i]];
-                    if(!obj) return null;
-                }
-                return obj;
-            };
-            
-            var initialize = function () {
-                if(scope.allObjects && scope.selectedObjects && !initialized){
-                    initialized = true;
-                    for(var i=0; i<scope.allObjects.length; i++){
-                        if(findInList(scope.allObjects[i], scope.selectedObjects) != -1){
-                            scope.allObjects[i]['selected'] = true;
-                        }else{
-                            scope.allObjects[i]['selected'] = false;
-                        }
+                scope.$watch('selectedObjects', function () {
+                    initialize();
+                });
+
+                var getValueFromKeyChain = function (obj, chain) {
+                    for(var i=0; i<chain.length; i++){
+                        obj=obj[chain[i]];
+                        if(!obj) return null;
                     }
-                    if(splitBy){
-                        var tree = {};
-                        var branches = [];
-                        var keyChain = splitBy.split('.');
-                        for(var j=0; j<scope.allObjects.length; j++){
-                            var category = getValueFromKeyChain(scope.allObjects[j], keyChain);
-                            if(category){
-                                if(!tree[category]){
-                                    tree[category] = [];
-                                    branches.push(category);
-                                }
-                                tree[category].push(scope.allObjects[j]);
+                    return obj;
+                };
+
+                var initialize = function () {
+                    if(scope.allObjects && scope.selectedObjects && !initialized){
+                        initialized = true;
+                        for(var i=0; i<scope.allObjects.length; i++){
+                            if(findInList(scope.allObjects[i], scope.selectedObjects) != -1){
+                                scope.allObjects[i]['selected'] = true;
+                            }else{
+                                scope.allObjects[i]['selected'] = false;
                             }
                         }
-                        scope.branches = branches;
-                        scope.tree = tree;
+                        if(splitBy){
+                            var tree = {};
+                            var branches = [];
+                            var keyChain = splitBy.split('.');
+                            for(var j=0; j<scope.allObjects.length; j++){
+                                var category = getValueFromKeyChain(scope.allObjects[j], keyChain);
+                                if(category){
+                                    if(!tree[category]){
+                                        tree[category] = [];
+                                        branches.push(category);
+                                    }
+                                    tree[category].push(scope.allObjects[j]);
+                                }
+                            }
+                            scope.branches = branches;
+                            scope.tree = tree;
+                        }
                     }
-                }
-            };
+                };
 
-            scope.refreshSelected = function () {
-                scope.selectedObjects = [];
-                for(var i=0; i<scope.allObjects.length; i++){
-                    if(scope.allObjects[i]['selected']){
-                        scope.selectedObjects.push(scope.allObjects[i]);
+                scope.refreshSelected = function () {
+                    scope.selectedObjects = [];
+                    for(var i=0; i<scope.allObjects.length; i++){
+                        if(scope.allObjects[i]['selected']){
+                            scope.selectedObjects.push(scope.allObjects[i]);
+                        }
                     }
-                }
-                console.log(scope.selectedObjects);
-            };
-            
-            var findInList = function (toFind, list) {
-                for(var i=0; i<list.length; i++){
-                    if(toFind[trackBy]==list[i][trackBy]){
-                        return i;
+                    console.log(scope.selectedObjects);
+                };
+
+                var findInList = function (toFind, list) {
+                    for(var i=0; i<list.length; i++){
+                        if(toFind[trackBy]==list[i][trackBy]){
+                            return i;
+                        }
                     }
-                }
-                return -1;
-            };
-        }
-    };
-});
+                    return -1;
+                };
+            }
+        };
+    });
+})();
