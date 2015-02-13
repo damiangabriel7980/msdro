8c417e2f957c232b4b89613d141c86fb42220cd9
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-02-13 14:10:19 +0200

    Added chat features

--- app/models/MSDDoc_chat.js
+++ app/models/MSDDoc_chat.js
@@ -2,13 +2,19 @@
  * Created by miricaandrei23 on 20.01.2015.
  */
 var mongoose		= require('mongoose');
+var deepPopulate = require('mongoose-deep-populate');
 var Schema			= mongoose.Schema;
 
 var ChatDocSchema = new Schema({
     post:{type: Schema.Types.ObjectId,ref:'MSDDoc_news_post'},
     participants: [{type: Schema.Types.ObjectId,ref: 'User'}],
     subscribers: [{type: Schema.Types.ObjectId,ref: 'User'}],
+    last_message: {type: Schema.Types.ObjectId,ref: 'MSDDoc_messages'},
     created:Date
+});
+
+ChatDocSchema.plugin(deepPopulate, {
+    whitelist: ['post.owner']
 });
 
 module.exports = mongoose.model('MSDDoc_chat',ChatDocSchema,'MSDDoc_chat');
