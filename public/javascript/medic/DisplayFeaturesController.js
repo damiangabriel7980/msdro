33a2a7ae265766eca9393f6369fd71990ec0d005
miricaandrei23 <mirica.andrei23@yahoo.com>
2015-02-18 17:33:17 +0200

    Changed url for Januvia

--- public/javascript/medic/DisplayFeaturesController.js
+++ public/javascript/medic/DisplayFeaturesController.js
@@ -1,6 +1,6 @@
 cloudAdminControllers.controller('DisplayFeaturesController', ['$scope', '$rootScope', '$stateParams', '$sce', 'SpecialFeaturesService', function($scope, $rootScope, $stateParams, $sce, SpecialFeaturesService){
     $scope.featureSrc = "";
     switch ($stateParams.feature){
-        case "Januvia": $scope.featureSrc = $sce.trustAsResourceUrl("http://msd-ionic.herokuapp.com/"); break;
+        case "Januvia": $scope.featureSrc = $sce.trustAsResourceUrl("https://msd-ionic.herokuapp.com/"); break;
     }
 }]);
