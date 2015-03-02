ac1a0a221da7e3b3c994cfcd8527e989ac0cc827
miricaandrei23 <mirica.andrei23@yahoo.com>
2015-03-02 14:02:53 +0200

    Intro showed only once on Home page

--- public/javascript/medic/MainController.js
+++ public/javascript/medic/MainController.js
@@ -1,4 +1,55 @@
-controllers.controller('MainController', ['$scope', '$state', '$modal', function ($scope, $state, $modal) {
+controllers.controller('MainController', ['$scope', '$state', '$modal','$rootScope', function ($scope, $state, $modal,$rootScope) {
+    console.log(localStorage);
+    var changeGroupModalStatus= function(groupID,value){
+        var retrievedObject = sessionStorage.getItem('statusModalGroups');
+        var statusModals = JSON.parse(retrievedObject);
+        statusModals[groupID] = value;
+        sessionStorage.setItem('statusModalGroups',JSON.stringify(statusModals));
+    };
+    $rootScope.$watch('specialGroups',function(){
+        if(!localStorage.statusModalGroups)
+        {
+            var modalGroups={};
+            for(var i=0;i<$rootScope.specialGroups.length;i++)
+            {
+                modalGroups[$rootScope.specialGroups[i]._id]=true;
+                console.log(modalGroups);
+            }
+            //if(sessionStorage.statusModalGroups)
+            //    sessionStorage.removeItem('statusModalGroups');
+            localStorage.setItem('statusModalGroups',JSON.stringify(modalGroups));
+            $modal.open({
+                templateUrl: 'partials/medic/modals/presentationModal.html',
+                size: 'lg',
+                keyboard: false,
+                backdrop: 'static',
+                windowClass: 'fade',
+                controller: 'PresentationModal'
+            });
+        }
+        else
+        {
+            if(JSON.parse(localStorage.getItem('statusModalGroups'))[$rootScope.specialGroupSelected._id]===true)
+            {
+                if(JSON.parse(sessionStorage.getItem('statusModalGroups'))[$rootScope.specialGroupSelected._id]===true)
+                {
+                    changeGroupModalStatus($rootScope.specialGroupSelected._id,false);
+                    $modal.open({
+                        templateUrl: 'partials/medic/modals/presentationModal.html',
+                        size: 'lg',
+                        keyboard: false,
+                        backdrop: 'static',
+                        windowClass: 'fade',
+                        controller: 'PresentationModal'
+                    });
+                }
+            }
+            else
+            {
+                console.log('No Presentation!');
+            }
+        }
+    });
 
     $scope.showFarmaModal = function() {
         var modalInstance = $modal.open({
