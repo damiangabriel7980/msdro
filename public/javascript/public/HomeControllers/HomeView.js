460fb86ad6c329610d9e20c7fe16825913525254
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-03-03 18:10:32 +0200

    Removed unneeded directive

--- public/javascript/public/HomeControllers/HomeView.js
+++ public/javascript/public/HomeControllers/HomeView.js
@@ -59,17 +59,4 @@ controllers.controller('HomeView', ['$scope', '$rootScope', 'HomeService', '$sce
     return function(items) {
         return items.slice().reverse();
     };
-})
-    .directive('ngEnter', function () {
-        return function (scope, element, attrs) {
-            element.bind("keydown keypress", function (event) {
-                if (event.which === 13) {
-                    scope.$apply(function () {
-                        scope.$eval(attrs.ngEnter);
-                    });
-
-                    event.preventDefault();
-                }
-            });
-        };
-    });
+});
