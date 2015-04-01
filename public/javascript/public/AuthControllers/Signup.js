0e3c5cc13e15023cb6da19ce766b1d1d9b84b91e
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-04-01 19:48:17 +0300

    Use auth functions in single controller

--- public/javascript/public/AuthControllers/Signup.js
+++ public/javascript/public/AuthControllers/Signup.js
@@ -1,4 +1,36 @@
-controllers.controller('Signup', ['$scope', 'AuthService', function ($scope, AuthService) {
+app.controller('Signup', ['$scope', 'AuthService', '$window', 'Utils', function($scope, AuthService, $window, Utils) {
+
+    //================================================================================================== init variables
+    $scope.user = {
+        subscriptions: {
+            newsletterStaywell: false,
+            infoMSD: false
+        },
+        temp: {
+            proofFile: null
+        }
+    };
+
+    $scope.nonUser = {};
+
+    var lockSubmitting = false;
+
+    $scope.codeTooltip = "Deoarece statul roman, prin legea 95/2006 cu HCS nr 27_11.10.2013 " +
+        "reglementeaza publicitatea medicamentelor doar catre specialisti in domeniul sanatatii, " +
+        "va rugam sa introduceti codul care atesta dovada calitatii dvs de profesionist in domeniul " +
+        "sanatatii (medic/farmacist). Daca nu aveti un cod de activare, va rugam sa contactati " +
+        "un reprezentant MSD sau sa sunati la numarul de telefon ...";
+
+    $scope.practices = [
+        {
+            number: 1,
+            name: "Public"
+        },
+        {
+            number: 2,
+            name: "Privat"
+        }
+    ];
 
     $scope.titles = [
         {number: 1, name: "Dl"},
@@ -7,24 +39,132 @@ controllers.controller('Signup', ['$scope', 'AuthService', function ($scope, Aut
         {number: 4, name: "Dr"}
     ];
 
-    $scope.signup = function () {
-        console.log(this);
-        if(!(this.email && this.title && this.name && this.password && this.confirm)){
-            $scope.resetAlert("danger", "Toate campurile sunt obligatorii");
-        }else if(!this.terms) {
-            $scope.resetAlert("danger", "Trebuie sa acceptati termenii si conditiile pentru a continua");
-        }else if(this.password != this.confirm) {
-            $scope.resetAlert("danger", "Parolele nu corespund");
-        }else{
-            AuthService.signup.query({title: this.title, name: this.name, email: this.email, password: this.password, createdFromStaywell: true}).$promise.then(function (resp) {
-                if(resp.error){
-                    $scope.resetAlert("danger", resp.message);
-                }else{
-                    $scope.registeredAddress = resp.user;
-                    $scope.renderView("created");
+    //============================================================================================== profession / group
+
+    $scope.selectProfession = function () {
+        AuthService.specialGroups.query({profession: $scope.user.profession}).$promise.then(function (response) {
+            $scope.groups = response;
+            $scope.user.groupsID = response[0]._id;
+            lockSubmitting = false;
+        });
+    };
+
+    AuthService.professions.query().$promise.then(function (response) {
+        lockSubmitting = true;
+        $scope.professions = response;
+        $scope.user.profession = response[0]._id;
+        $scope.selectProfession();
+    });
+
+    //=================================================================================================== county / city
+
+    var resetCities = function () {
+        $scope.cities = [];
+        $scope.city = {
+            selected: {}
+        };
+    };
+    var resetCounties = function () {
+        $scope.counties = [];
+        $scope.county = {
+            selected: {}
+        };
+    };
+    resetCities();
+    resetCounties();
+
+    $scope.$watch('county.selected', function () {
+        if($scope.county.selected){
+            resetCities();
+            AuthService.cities.query({county: $scope.county.selected._id}).$promise.then(function (resp) {
+                if(resp.success){
+                    $scope.cities = resp.success.sort(function(a,b){
+                        if ( a.name < b.name )
+                            return -1;
+                        if ( a.name > b.name )
+                            return 1;
+                        return 0;
+                    });
                 }
-            })
+            });
+        }
+    });
+
+    // get counties and cities
+    AuthService.counties.query().$promise.then(function (resp) {
+        $scope.counties = resp.success;
+    });
+
+    //====================================================================================================== load proof
+
+    $scope.proofSelected = function ($files, $event) {
+        if(($files||[])[0]){
+            $scope.user.temp.proofFile = $files[0];
         }
     };
 
-}]);
+    //========================================================================================================== submit
+
+    $scope.completeProfile = function () {
+        AuthService.completeProfile(this, function (err, resp) {
+            if(err){
+                $scope.resetAlert("danger", err);
+            }else{
+                if(resp.error){
+                    $scope.resetAlert("danger", resp.message);
+                }else{
+                    if(resp.state === "ACCEPTED"){
+                        $window.location.href = "pro";
+                    }else{
+                        $scope.renderView("profileCompleted", {registeredAddress: resp.user});
+                    }
+                }
+            }
+        });
+    };
+
+    $scope.createAccount = function () {
+        AuthService.createAccount(this, function (err, resp) {
+            if(err){
+                $scope.resetAlert("danger", err);
+            }else{
+                if(resp.error){
+                    $scope.resetAlert("danger", resp.message);
+                }else{
+                    $scope.renderView("created", {registeredAddress: resp.user});
+                }
+            }
+        });
+    };
+
+}]).filter('propsFilter', function() {
+    //used for select2
+    return function(items, props) {
+        var out = [];
+
+        if (angular.isArray(items)) {
+            items.forEach(function(item) {
+                var itemMatches = false;
+
+                var keys = Object.keys(props);
+                for (var i = 0; i < keys.length; i++) {
+                    var prop = keys[i];
+                    var text = props[prop].toLowerCase();
+                    if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+                        itemMatches = true;
+                        break;
+                    }
+                }
+
+                if (itemMatches) {
+                    out.push(item);
+                }
+            });
+        } else {
+            // Let the output be the input untouched
+            out = items;
+        }
+
+        return out;
+    }
+});
