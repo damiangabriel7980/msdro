4ea37c31ee9b417c191804695cb9e9f58ca4f1e6
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-04-01 14:39:41 +0300

    Break down auth functions

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ var devServer = http.createServer(app);
 // api ======================================================================
 require('./app/api.js')(app, sessionSecret, email, logger, pushServerAddr, amazon, express.Router()); // load our private routes and pass in our app and session secret
 require('./app/apiPublic.js')(app, email, express.Router()); // load our public routes and pass in our app
-require('./app/apiGloballyShared.js')(app, email, logger, express.Router());
+require('./app/apiGloballyShared.js')(app, email, logger, amazon, express.Router());
 require('./app/apiMobileShared.js')(app, email, logger, tokenSecret, pushServerAddr, express.Router());
 require('./app/apiConferences.js')(app, email, logger, tokenSecret, pushServerAddr, express.Router());
 require('./app/apiMSDDoc.js')(app, logger, tokenSecret, secureServer, express.Router());
