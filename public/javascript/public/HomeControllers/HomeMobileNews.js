04aaa4b1a10e3d004f3fd72c7b62377d5462f26c
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-05-22 18:13:09 +0300

    Navigate from mobile home

--- public/javascript/public/HomeControllers/HomeMobileNews.js
+++ public/javascript/public/HomeControllers/HomeMobileNews.js
@@ -1,22 +1,29 @@
-controllers.controller('HomeMobileNews', ['$scope', '$rootScope', 'ContentService', '$sce', function($scope, $rootScope, ContentService, $sce) {
+controllers.controller('HomeMobileNews', ['$scope', '$state', 'ContentService', function($scope, $state, ContentService) {
 
     ContentService.mobileContent.query().$promise.then(function (resp) {
         if(resp.success){
             var categories = [];
             categories.push({
                 name: "ULTIMELE NOUTATI",
-                content: resp.success.news
+                content: resp.success.news,
+                sref: "stiri.detail"
             });
             categories.push({
                 name: "CELE MAI CITITE ARTICOLE",
-                content: resp.success.articles
+                content: resp.success.articles,
+                sref: "articole.detail"
             });
             categories.push({
                 name: "DOWNLOADS",
-                content: resp.success.downloads
+                content: resp.success.downloads,
+                sref: "downloads.detail"
             });
             $scope.categories = categories;
         }
     });
+
+    $scope.navigateTo = function (sref, params) {
+        $state.go(sref, params);
+    }
 
 }]);
