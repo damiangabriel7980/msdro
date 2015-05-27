678ea3903bd89d3bfa92de060e70ac98a6533dff
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-05-27 10:31:51 +0300

    get from_email from system parameters

--- app/modules/mailer/index.js
+++ app/modules/mailer/index.js
@@ -5,22 +5,32 @@ var mandrill = require('node-mandrill')(my_config.mandrillKey);
 
 var Q = require('q');
 
+var Parameters = require('../../models/parameters');
+
 exports.send = function (template_name, template_content, to, subject) {
     var deferred = Q.defer();
-    mandrill('/messages/send-template', {
-        "template_name": template_name,
-        "template_content": template_content,
-        "message": {
-            from_email: 'adminMSD@qualitance.ro',
-            to: to,
-            subject: subject
-        }
-    }, function(err){
+    //get from_email from system parameters
+    Parameters.findOne({name: "FROM_EMAIL"}, function (err, param) {
         if(err){
-            console.log(err);
-            deferred.reject("Eroare la trimitere email");
+            deferred.reject(err);
         }else{
-            deferred.resolve();
+            //send email
+            mandrill('/messages/send-template', {
+                "template_name": template_name,
+                "template_content": template_content,
+                "message": {
+                    from_email: param.value || param.default_value,
+                    to: to,
+                    subject: subject
+                }
+            }, function(err){
+                if(err){
+                    console.log(err);
+                    deferred.reject("Eroare la trimitere email");
+                }else{
+                    deferred.resolve();
+                }
+            });
         }
     });
     return deferred.promise;
