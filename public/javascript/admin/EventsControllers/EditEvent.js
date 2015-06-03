7c59aa6934727561caf07f839a135aaec726f37d
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-06-03 12:45:15 +0300

    More editing options for events' description

--- public/javascript/admin/EventsControllers/EditEvent.js
+++ public/javascript/admin/EventsControllers/EditEvent.js
@@ -101,4 +101,14 @@ controllers.controller('EditEvent', ['$scope', '$state', '$stateParams', 'Events
         })
     };
 
+    $scope.tinymceOptions = {
+        selector: "textarea",
+        plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table contextmenu paste charmap"
+        ],
+        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+    };
+
 }]);
