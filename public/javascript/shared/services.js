e48c06a2f168fff45323fd6e2ea4b902d66296c3
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-06-17 10:52:34 +0300

    Cookies service

--- public/javascript/shared/services.js
+++ public/javascript/shared/services.js
@@ -218,3 +218,24 @@ services.factory('therapeuticAreas', ['$resource', function($resource){
         }
     }
 }]);
+services.factory('CookiesService', function () {
+    return {
+        setCookie: function(cname, cvalue) {
+            var now = new Date(),
+            // this will set the expiration to 6 months
+                exp = new Date(now.getFullYear(), now.getMonth()+6, now.getDate());
+            var expires = "expires="+exp.toUTCString();
+            document.cookie = cname + "=" + cvalue + "; " + expires;
+        },
+        getCookie: function(cname) {
+            var name = cname + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0; i<ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1);
+                if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+            }
+            return "";
+        }
+    }
+});
