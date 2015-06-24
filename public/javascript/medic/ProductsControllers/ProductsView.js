7b8f6b97ec3e7e6ef5826753632c780ffc7b4f69
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-06-24 18:45:42 +0300

    Code indentation

--- public/javascript/medic/ProductsControllers/ProductsView.js
+++ public/javascript/medic/ProductsControllers/ProductsView.js
@@ -7,8 +7,6 @@
  *
  * @requires $scope
  * */
-
-
 controllers.controller('ProductsView', ['$scope','$rootScope' ,'ProductService','$stateParams','$sce','$window','$timeout', function($scope,$rootScope,ProductService,$stateParams,$sce,$window,$timeout){
     window.scrollTo(0,0);
     $scope.lmt=8;
@@ -22,21 +20,21 @@ controllers.controller('ProductsView', ['$scope','$rootScope' ,'ProductService',
     $scope.allLetters = {};
 
     ProductService.getByArea.query({id:$stateParams.id, specialGroup: $rootScope.specialGroupSelected?$rootScope.specialGroupSelected._id.toString():null}).$promise.then(function(result){
-            $scope.products = result;
-            $scope.allLetters['A-Z'] = $scope.products;
-            $scope.products.forEach(function (item) {
-                var firstLetter = item.name.charAt(0);
-                if (!$scope.firstLetters[firstLetter]) {
-                    $scope.firstLetters[firstLetter] = [];
-                }
-                $scope.firstLetters[firstLetter].push(item);
-            });
-
+        $scope.products = result;
+        $scope.allLetters['A-Z'] = $scope.products;
+        $scope.products.forEach(function (item) {
+            var firstLetter = item.name.charAt(0);
+            if (!$scope.firstLetters[firstLetter]) {
+                $scope.firstLetters[firstLetter] = [];
+            }
+            $scope.firstLetters[firstLetter].push(item);
         });
+
+    });
     $scope.filterResults=function(key){
         if(key === 'A-Z')
             $scope.products = $scope.allLetters[key];
         else
             $scope.products = $scope.firstLetters[key];
     };
-  }]);
+}]);
