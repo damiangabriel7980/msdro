101772e168422af332030365e11ab5aa1b412b16
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-06-24 20:16:14 +0300

    Self-called function implemented in different modules to prevent global variables name conflicts

--- public/modules/horizontal-content-list/directive.js
+++ public/modules/horizontal-content-list/directive.js
@@ -1,29 +1,31 @@
-var scripts = document.getElementsByTagName("script");
-var currentScriptPath = scripts[scripts.length-1].src;
+(function() {
+    var scripts = document.getElementsByTagName("script");
+    var currentScriptPath = scripts[scripts.length-1].src;
 
-angular.module('horizontalContentList', []).directive('horizontalContentList', ['Utils', function(Utils) {
-    return {
-        restrict: 'E',
-        templateUrl: currentScriptPath.replace('directive.js', 'template.html'),
-        scope: {
-            content: '=',
-            imagePathAttr: '@',
-            imagePathPrefix: '@',
-            defaultImagePath: '@',
-            titleAttr: '@',
-            textAttr: '@',
-            boldAttr1: '@',
-            boldAttr2: '@',
-            dateAttr: '@',
-            buttonLabel: '@',
-            showDivider: '=',
-            navigate: '='
-        },
-        link: function(scope, element, attrs) {
-            // expose functions
-            scope.trustAsHtml = Utils.trustAsHtml;
-            scope.createHeader = Utils.createHeader;
-            scope.withBackground = attrs.hasOwnProperty("withBackground");
-        }
-    };
-}]);
+    angular.module('horizontalContentList', []).directive('horizontalContentList', ['Utils', function(Utils) {
+        return {
+            restrict: 'E',
+            templateUrl: currentScriptPath.replace('directive.js', 'template.html'),
+            scope: {
+                content: '=',
+                imagePathAttr: '@',
+                imagePathPrefix: '@',
+                defaultImagePath: '@',
+                titleAttr: '@',
+                textAttr: '@',
+                boldAttr1: '@',
+                boldAttr2: '@',
+                dateAttr: '@',
+                buttonLabel: '@',
+                showDivider: '=',
+                navigate: '='
+            },
+            link: function(scope, element, attrs) {
+                // expose functions
+                scope.trustAsHtml = Utils.trustAsHtml;
+                scope.createHeader = Utils.createHeader;
+                scope.withBackground = attrs.hasOwnProperty("withBackground");
+            }
+        };
+    }]);
+})();
--- public/modules/vertical-content-list/directive.js
+++ public/modules/vertical-content-list/directive.js
@@ -1,24 +1,26 @@
-var scripts = document.getElementsByTagName("script");
-var currentScriptPath = scripts[scripts.length-1].src;
+(function() {
+    var scripts = document.getElementsByTagName("script");
+    var currentScriptPath = scripts[scripts.length-1].src;
 
-angular.module('verticalContentList', []).directive('verticalContentList', ['Utils', function(Utils) {
-    return {
-        restrict: 'E',
-        templateUrl: currentScriptPath.replace('directive.js', 'template.html'),
-        scope: {
-            content: '=',
-            imagePathAttr: '@',
-            imagePathPrefix: '@',
-            defaultImagePath: '@',
-            titleAttr: '@',
-            textAttr: '@',
-            navigate: '='
-        },
-        link: function(scope, element, attrs) {
-            // expose functions
-            scope.trustAsHtml = Utils.trustAsHtml;
-            scope.convertAndTrustAsHtml = Utils.convertAndTrustAsHtml;
-            scope.withBackground = attrs.hasOwnProperty("withBackground");
-        }
-    };
-}]);
+    angular.module('verticalContentList', []).directive('verticalContentList', ['Utils', function(Utils) {
+        return {
+            restrict: 'E',
+            templateUrl: currentScriptPath.replace('directive.js', 'template.html'),
+            scope: {
+                content: '=',
+                imagePathAttr: '@',
+                imagePathPrefix: '@',
+                defaultImagePath: '@',
+                titleAttr: '@',
+                textAttr: '@',
+                navigate: '='
+            },
+            link: function(scope, element, attrs) {
+                // expose functions
+                scope.trustAsHtml = Utils.trustAsHtml;
+                scope.convertAndTrustAsHtml = Utils.convertAndTrustAsHtml;
+                scope.withBackground = attrs.hasOwnProperty("withBackground");
+            }
+        };
+    }]);
+})();
