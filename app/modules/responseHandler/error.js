e723a5ba2740eecce06dc327f1c1c6f689ae614f
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-07-01 16:50:23 +0300

    Send error/success messages as codes

--- app/modules/responseHandler/error.js
+++ app/modules/responseHandler/error.js
@@ -1,12 +1,16 @@
+var errorCodes = {
+    0: "Eroare necunoscuta"
+};
+
 module.exports = function(logger){
-    return function(res, error ,status, message){
+    return function(res, error ,status, code){
         logger.log(error);
         if(status === 500)
         {
             res.send(500,{error: 'A aparut o eroare pe server!'});
         }
         else{
-            res.send(status,{error: message})
+            res.send(status,{error: errorCodes[code || 0]})
         }
     };
 };
--- app/modules/responseHandler/success.js
+++ app/modules/responseHandler/success.js
@@ -1,5 +1,9 @@
+var successCodes = {
+    0: "Cererea a fost procesata cu succes"
+};
+
 module.exports = function(logger){
-    return function(res, object, message){
-        res.send({success: object, message: message});
+    return function(res, object, code){
+        res.send({success: object, message: successCodes[code || 0]});
     };
 };
