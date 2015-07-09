705c24122668e6341438328e9a3d4c1340e6cc17
miricaandrei2392 <mirica.andrei23@yahoo.com>
2015-07-09 11:24:42 +0300

    Disabled IE caching

--- public/javascript/medic/app.js
+++ public/javascript/medic/app.js
@@ -30,7 +30,14 @@ app.config(['$locationProvider', function($location) {
     $location.hashPrefix('!');
 }]);
 
-app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+app.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider,$httpProvider) {
+    $httpProvider.defaults.cache = false;
+    if (!$httpProvider.defaults.headers.get) {
+        $httpProvider.defaults.headers.get = {};
+    }
+    // disable IE ajax request caching
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
+
     $urlRouterProvider.otherwise("/");
     $stateProvider
         .state('home',{
