fb099b79019d93ec22b79312152a505577692c0c
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-07-20 16:26:17 +0300

    Validate area name

--- public/javascript/admin/TherapeuticAreasControllers/AddTherapeuticAreas.js
+++ public/javascript/admin/TherapeuticAreasControllers/AddTherapeuticAreas.js
@@ -4,7 +4,7 @@
 /**
  * Created by miricaandrei23 on 25.11.2014.
  */
-controllers.controller('AddTherapeuticAreas', ['$scope','$rootScope' ,'areasAdminService','$stateParams','$sce','$filter','$modalInstance','$state', 'Success', 'Error', function($scope,$rootScope,areasAdminService,$stateParams,$sce,$filter,$modalInstance,$state,Success,Error){
+controllers.controller('AddTherapeuticAreas', ['$scope','$rootScope' ,'areasAdminService','$sce','$filter','$modalInstance','$state', 'Success', 'Error', function($scope,$rootScope,areasAdminService,$sce,$filter,$modalInstance,$state,Success,Error){
 
     $scope.resetAlert = function (message, type) {
         $scope.therapeuticAlert = {
@@ -33,15 +33,19 @@ controllers.controller('AddTherapeuticAreas', ['$scope','$rootScope' ,'areasAdmi
     });
 
     $scope.takeAction = function(){
-        var area = this.arie;
+        var area = this.arie || {};
         if(area['therapeutic-areasID']) area['therapeutic-areasID'] = [area['therapeutic-areasID']];
-        //console.log(area);
-        areasAdminService.areas.create(area).$promise.then(function(){
-            $state.reload();
-            $modalInstance.close();
-        }).catch(function(err){
-            $scope.resetAlert(Error.getMessage(err));
-        });
+        if(area.name){
+            //console.log(area);
+            areasAdminService.areas.create(area).$promise.then(function(){
+                $state.reload();
+                $modalInstance.close();
+            }).catch(function(err){
+                $scope.resetAlert(Error.getMessage(err));
+            });
+        }else{
+            $scope.resetAlert("Numele ariei terapeutice este obligatoriu");
+        }
     };
 
     $scope.closeModal = function () {
