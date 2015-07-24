0a5680d6ae98a3b65570dfe16f413779b2abe5e1
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-07-24 16:42:26 +0300

    #MS-289

--- public/javascript/medic/ProfileControllers/Profile.js
+++ public/javascript/medic/ProfileControllers/Profile.js
@@ -1,7 +1,7 @@
 /**
  * Created by andrei on 12.11.2014.
  */
-app.controllerProvider.register('Profile', ['$scope', '$rootScope', 'ProfileService', 'therapeuticAreas' , '$sce', '$timeout', '$state', 'Utils', 'IntroService', 'Success', 'Error', 'Validations', function($scope, $rootScope, ProfileService, therapeuticAreas, $sce, $timeout,$state, Utils, IntroService, Success, Error, Validations){
+app.controllerProvider.register('Profile', ['$scope', '$rootScope', 'ProfileService', 'therapeuticAreas' , '$sce', '$timeout', '$state', 'Utils', 'IntroService', 'Success', 'Error', 'Validations', 'SpecialFeaturesService', function($scope, $rootScope, ProfileService, therapeuticAreas, $sce, $timeout,$state, Utils, IntroService, Success, Error, Validations, SpecialFeaturesService){
 
     //===================================================================== init variables
     var imagePre = $rootScope.pathAmazonDev;
@@ -152,7 +152,9 @@ app.controllerProvider.register('Profile', ['$scope', '$rootScope', 'ProfileServ
     $scope.submitProfileForm = function (isValid) {
         if(isValid){
             if(this.rememberOption){
-                IntroService.hideNextTime.resetStatus();
+                SpecialFeaturesService.specialGroups.getSelected().then(function (group) {
+                    IntroService.hideNextTime.setStatus(group._id, false);
+                });
             }
             var toSend = {};
             toSend.name = this.fullname;
