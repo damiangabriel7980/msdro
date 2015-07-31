7c1ab51b1fadc3d0aa60f9f55ca1d40532459b27
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-07-31 12:15:40 +0300

    get date of last users modification

--- app/apiJanuvia.js
+++ app/apiJanuvia.js
@@ -16,6 +16,18 @@ module.exports = function(app, logger, router) {
 		});
 	})
 
+	router.route('/last_modified').get(function (req, res) {
+		JanuviaUsers.find({}, {last_modified: 1}).sort({last_modified: -1}).limit(1).exec(function (err, users) {
+			if(err){
+				handleError(res, err);
+			}else if(users.length === 0){
+				handleError(res, false, 404, 1);
+			}else{
+				handleSuccess(res, users[0].last_modified);
+			}
+		});
+	});
+
 
 	app.use('/apiJanuvia', router);
 }
