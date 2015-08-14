d203e483c9e028dcefc205692e6ac0e7413dc30c
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-08-14 19:20:17 +0300

    Added campaign stats

--- public/javascript/admin/NewsletterControllers/NewsletterCampaignSummary.js
+++ public/javascript/admin/NewsletterControllers/NewsletterCampaignSummary.js
@@ -1,8 +1,8 @@
 controllers.controller('NewsletterCampaignSummary', ['$scope', 'NewsletterService', 'campaign_id', 'Success', function ($scope, NewsletterService, campaign_id, Success) {
 
     NewsletterService.statistics.query({campaign: campaign_id}).$promise.then(function (resp) {
-        var stats = Success.getObject(resp);
-        console.log(stats);
+        $scope.statistics = Success.getObject(resp);
+        console.log($scope.statistics);
     })
 
 }]);
