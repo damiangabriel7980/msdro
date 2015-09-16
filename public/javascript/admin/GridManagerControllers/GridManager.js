e5513124b2f693e58a2060d7f70f7413a93eb233
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-09-16 14:52:09 +0300

    tinyMCE customisation info

--- public/javascript/admin/GridManagerControllers/GridManager.js
+++ public/javascript/admin/GridManagerControllers/GridManager.js
@@ -1,8 +1,23 @@
 controllers.controller('GridManager', ['$scope', function($scope){
 	$(document).ready(function(){
         $("#mgrid").gridmanager({
-        	debug: 1,
-        	rte: "ckeditor"
+        	debug: 1
+        	//for tinyMCE customisation insert options here like:
+        	//tinymce: { config: {options}}
+        	//default looks like:`
+        	/*
+        	tinymce: {
+        	    config: {
+        	      inline: true,
+        	      plugins: [
+        	      "advlist autolink lists link image charmap print preview anchor",
+        	      "searchreplace visualblocks code fullscreen",
+        	      "insertdatetime media table contextmenu paste"
+        	      ],
+        	      toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        	    }
+        	}
+        	 */
         });
     });
 }]);
