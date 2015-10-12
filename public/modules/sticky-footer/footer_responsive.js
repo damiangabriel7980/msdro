d5c7753eb8942de7d250b6eb7a334c189a2f6bbb
miricaandrei2392 <mirica.andrei23@yahoo.com>
2015-10-12 18:02:56 +0300

    New strategy for footer position + Back to Courses button

--- public/modules/sticky-footer/footer_responsive.js
+++ public/modules/sticky-footer/footer_responsive.js
@@ -3,12 +3,13 @@
  */
 (function() {
     'use strict';
-    angular.module('footerResponsive', []).directive('footerResponsive', ['$window', '$timeout', function($window, $timeout) {
+    angular.module('footerResponsive', []).directive('footerResponsive', ['$window', '$timeout', '$document', function($window, $timeout, $document) {
         return {
         restrict: 'EA',
             templateUrl: function(element, attr) {
                 return attr.templateLink;
             },
+            priority: -1000,
             transclude: true,
             scope: {
             stickBelowId: '@',
@@ -17,25 +18,35 @@
         link: function (scope, element, attrs) {
             var footerElement;
             var content;
-            var calculatePadding = function(){
-                footerElement = angular.element(element[0].firstChild);
+            footerElement = angular.element(element[0].firstChild);
+            footerElement.hide();
+            var checkScroll = function(){
                 content = angular.element('#'+ scope.stickBelowId)[0];
-                var heightOfContent = content.offsetTop + content.offsetHeight;
-                var footerHeight = element[0].firstChild.offsetHeight;
-                var windowHeight = angular.element($window)[0].innerHeight;
-                if(heightOfContent + footerHeight > windowHeight)
-                    footerElement.css('margin-top', 0);
-                else
-                    footerElement.css('margin-top', windowHeight - heightOfContent - footerHeight);
+                scope.heightOfContent = content.offsetTop + content.offsetHeight;
+                if (angular.element($document).height() <= (angular.element($window).height() + angular.element($window).scrollTop()) + 86){
+                    footerElement.show();
+                    footerElement.css({'position': 'fixed','bottom':0});
+                }
+                else if(angular.element($document).height() > (angular.element($window).height()))
+                {
+                    footerElement.hide();
+                    footerElement.css({'position': 'relative','bottom':0});
+                }
             };
 
+
             var continuousRefresh = function () {
-                calculatePadding();
+                checkScroll();
                 $timeout(continuousRefresh, 300);
             };
 
+            angular.element($window).scroll(function () {
+                //appliedCheck();
+                checkScroll();
+            });
+
             angular.element($window).bind('resize', function () {
-                calculatePadding();
+                checkScroll();
                 scope.$apply();
             });
 
