638b8a411cbbf1ecb544440690e0a7cbbd480753
miricaandrei2392 <mirica.andrei23@yahoo.com>
2015-10-12 18:29:45 +0300

    Hot-fix : footer performance

--- public/modules/sticky-footer/footer_responsive.js
+++ public/modules/sticky-footer/footer_responsive.js
@@ -19,31 +19,24 @@
             var footerElement;
             var content;
             footerElement = angular.element(element[0].firstChild);
+            footerElement.css({'position': 'fixed','bottom':0});
             footerElement.hide();
             var checkScroll = function(){
                 content = angular.element('#'+ scope.stickBelowId)[0];
                 scope.heightOfContent = content.offsetTop + content.offsetHeight;
-                if (angular.element($document).height() <= (angular.element($window).height() + angular.element($window).scrollTop()) + 86){
+                if (angular.element($document).height() <= (angular.element($window).height() + angular.element($window).scrollTop()) + 60){
                     footerElement.show();
-                    footerElement.css({'position': 'fixed','bottom':0});
                 }
                 else if(angular.element($document).height() > (angular.element($window).height()))
                 {
                     footerElement.hide();
-                    footerElement.css({'position': 'relative','bottom':0});
                 }
             };
 
-
             var continuousRefresh = function () {
                 checkScroll();
-                $timeout(continuousRefresh, 300);
+                $timeout(continuousRefresh, 250);
             };
-
-            angular.element($window).scroll(function () {
-                //appliedCheck();
-                checkScroll();
-            });
 
             angular.element($window).bind('resize', function () {
                 checkScroll();
