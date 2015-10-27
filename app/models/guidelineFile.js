6fb84a1eedd18f1944022519a555043bb13a7502
alexandru.oprisean <alexandru.oprisean@qualitance.ro>
2015-10-27 19:22:05 +0200

    added enabled field

--- app/models/guidelineFile.js
+++ app/models/guidelineFile.js
@@ -7,10 +7,12 @@ var Schema = mongoose.Schema;
 var guidelineFileSchema = new Schema({
     guidelineFileUrl : String,
     displayName:String,
+    actualName:String,
     type : {type:String,default:'PDF'},
     creationDate: Date,
     lastModified: Date,
-    guidelineCategoryId:{type:Schema.Types.ObjectId,ref:'guidelineCategory'}
+    guidelineCategoryId:{type:Schema.Types.ObjectId,ref:'guidelineCategory'},
+    enabled:{type:Boolean,default:true}
 });
 
 module.exports = mongoose.model('guidelineFile',guidelineFileSchema,'guidelineFile');
