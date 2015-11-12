99ca96bb0f5517afa08195d4f2a02bd4f3f695d8
alexandru.oprisean <alexandru.oprisean@qualitance.ro>
2015-11-12 15:42:13 +0200

    controllers for conference list page and conference page

---
+++ public/javascript/medic/LiveTransmissionsControllers/conferenceController.js
@@ -0,0 +1,65 @@
+app.controllerProvider.register('ConferenceController',['$scope','ActionModal',function($scope,ActionModal){
+
+ $scope.menuToggled = false;
+
+  navigator.getUserMedia = ( navigator.getUserMedia ||
+                    navigator.webkitGetUserMedia ||
+                    navigator.mozGetUserMedia ||
+                    navigator.msGetUserMedia);
+
+   var webcamStream;
+
+   function startWebcam() {
+    if (navigator.getUserMedia) {
+    navigator.getUserMedia (
+
+     // constraints
+     {
+       audio: false,
+       video: true
+     },
+
+     // successCallback
+      function(localMediaStream) {
+       var video1 = document.getElementsByClassName('me')[0];
+       var video2 = document.getElementsByClassName('me')[1];
+       var video3 = document.getElementsByClassName('me')[2];
+       var video4 = document.getElementsByClassName('me')[3];
+       video1.src = window.URL.createObjectURL(localMediaStream);
+       video2.src = window.URL.createObjectURL(localMediaStream);
+       video3.src = window.URL.createObjectURL(localMediaStream);
+       video4.src = window.URL.createObjectURL(localMediaStream);
+       webcamStream = localMediaStream;
+     },
+
+     // errorCallback
+      function(err) {
+        console.log(err);
+        }
+ );
+   } else {
+     console.log("getUserMedia not supported");
+   }
+  }
+
+   function stopWebcam() {
+   webcamStream.stop();
+   }
+
+   startWebcam();
+
+   $scope.closeConference = function(){
+     ActionModal.show("Inchide-ti conferinta","Sunteti sigur ca doriti sa inchide-ti conferinta",function(){
+       console.log("conferinta Inchisa");
+     },{
+       yes:"Da"
+     })
+   };
+
+   $scope.toggleMenu = function(){
+     console.log("toggleMenu");
+     $scope.menuToggled = !$scope.menuToggled;
+   }
+
+   //make query to see the info for the conference
+}]);
---
+++ public/javascript/medic/LiveTransmissionsControllers/mainController.js
@@ -0,0 +1,31 @@
+app.controllerProvider.register('ConferenceListController',['$scope','$state','ConferenceService','Success','$modal',function($scope,$state,ConferenceService,Success,$modal){
+
+
+  console.log('asdasdasd');
+  var onInit = function(){
+    ConferenceService.conference.query().$promise.then(function(resp){
+      $scope.conferences = Success.getObject(resp);
+      console.log($scope.conferences);
+    }).catch(function(err){
+      console.log(err);
+    })
+  };
+
+  onInit();
+  $scope.openPreview = function(conference){
+    var modalInstance = $modal.open({
+      animation:true,
+      backdrop:'static',
+      keyboard:false,
+      templateUrl:'partials/medic/modals/previewModal.ejs',
+      controller:'previewModalController',
+      size:'lg',
+      resolve:{
+        conference:function(){
+          return conference
+        }
+      }
+    })
+  }
+
+}]);
