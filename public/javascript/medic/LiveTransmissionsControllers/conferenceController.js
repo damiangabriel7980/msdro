6e23763468ac2c0c94be377767214610a45f30e9
alexandru.oprisean <alexandru.oprisean@qualitance.ro>
2015-11-20 16:40:28 +0200

    fixes

---
+++ public/javascript/medic/LiveTransmissionsControllers/conferenceController.js
@@ -0,0 +1,79 @@
+app.controllerProvider.register('ConferenceController',['$scope','ActionModal',function($scope,ActionModal){
+
+ $scope.menuToggled = false;
+ $scope.showMessages = false;
+
+  navigator.getUserMedia = ( navigator.getUserMedia ||
+                    navigator.webkitGetUserMedia ||
+                    navigator.mozGetUserMedia ||
+                    navigator.msGetUserMedia);
+
+   var webcamStream;
+
+   function startWebcam() {
+    if (navigator.getUserMedia) {
+    navigator.getUserMedia (
+
+     // constraints
+     {
+       audio: false,
+       video: true
+     },
+
+     // successCallback
+      function(localMediaStream) {
+       var video1 = document.getElementsByClassName('me')[0];
+       var video2 = document.getElementsByClassName('me')[1];
+       var video3 = document.getElementsByClassName('me')[2];
+       var video4 = document.getElementsByClassName('me')[3];
+       video1.src = window.URL.createObjectURL(localMediaStream);
+       video2.src = window.URL.createObjectURL(localMediaStream);
+       video3.src = window.URL.createObjectURL(localMediaStream);
+       video4.src = window.URL.createObjectURL(localMediaStream);
+       webcamStream = localMediaStream;
+     },
+
+     // errorCallback
+      function(err) {
+        console.log(err);
+        }
+ );
+   } else {
+     console.log("getUserMedia not supported");
+   }
+  }
+
+   function stopWebcam() {
+   webcamStream.stop();
+   }
+
+   startWebcam();
+
+   $scope.closeConference = function(){
+     ActionModal.show("Inchide-ti conferinta","Sunteti sigur ca doriti sa inchide-ti conferinta",function(){
+       console.log("conferinta Inchisa");
+     },{
+       yes:"Da"
+     })
+   };
+
+   $scope.exitConferences = function(){
+     ActionModal.show("Iesiti din conferinta","Sunteti sigur ca doriti sa iesiti din conferinta",function(){
+       console.log("ati iesit din conferinta");
+     },{
+       yes:"Da"
+     })
+   };
+
+   $scope.toggleMenu = function(){
+     $scope.menuToggled = !$scope.menuToggled;
+   };
+
+   $scope.toggleMessages = function(){
+     console.log("show messages");
+     $scope.showMessages = !$scope.showMessages;
+
+   };
+
+   //make query to see the info for the conference
+}]);
