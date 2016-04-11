8b1afbf6f280abfb48ffefa3afc1a24496ed4685
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2016-04-11 17:15:47 +0300

    Moved conference preview functions inside modal controller

--- public/javascript/medic/ConferencesControllers/ConferencePreview.js
+++ public/javascript/medic/ConferencesControllers/ConferencePreview.js
@@ -1,7 +1,24 @@
-app.controllerProvider.register('ConferencePreview', ['$scope', '$modalInstance', 'videoUrl', 'conferenceData', function($scope, $modalInstance, videoUrl, conferenceData){
-	$scope.videoUrl = videoUrl;
+app.controllerProvider.register('ConferencePreview', ['$scope', '$sce', '$modalInstance', 'conferenceData', function($scope, $sce, $modalInstance, conferenceData){
+	getUserMedia(
+		{audio: true, video: true},
+		function(stream){
+			$scope.videoUrl = getVideoURL(stream)
+		},
+		function(err){
+			console.log(err);
+		}
+	)
 
 	$scope.startConference = function(){
 		$modalInstance.close(conferenceData.id);
 	}
+
+	function getVideoURL(stream){
+      if (window.URL) {
+        return $sce.trustAsResourceUrl(window.URL.createObjectURL(stream));
+      } else {
+        return $sce.trustAsResourceUrl(stream);
+      }
+    }
+
 }]);
