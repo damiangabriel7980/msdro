a1bdf3d193e0ef79537b3f164787955d3b8e72c3
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2016-04-19 14:04:11 +0300

    Open conference in a new tab

--- public/javascript/medic/ConferencesControllers/Conferences.js
+++ public/javascript/medic/ConferencesControllers/Conferences.js
@@ -63,14 +63,7 @@ app.controllerProvider.register('Conferences', ['$scope', '$http', 'ConferenceSe
 
     var startConference = function(conferenceId){
       console.log("Start conference: "+conferenceId);
-      //first we need to calculate our window dimensions and position
-      var width = window.innerWidth * 0.66 ;
-      var height = width * window.innerHeight / window.innerWidth ;
-      var top = (window.innerHeight - height) / 2;
-      var left = (window.innerWidth - width) / 2;
-      //open the window
-      window.open('/conference?id='+conferenceId, 'conferenceWindow', 'width=' + width + ', height=' + height + ', top=' + top + ', left=' + left);
-      //conference.join(conf._id);
+      window.open('/conference?id='+conferenceId);
     }
 
     function previewConference(conferenceId){
