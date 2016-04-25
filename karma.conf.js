fffcbd05d263dbabe3e13e90efda43c064bcff36
miricaandrei2392 <mirica.andrei23@yahoo.com>
2016-04-25 13:39:55 +0300

    Unit tests login

--- karma.conf.js
+++ karma.conf.js
@@ -1,53 +1,100 @@
 // Karma configuration
 // Generated on Tue Apr 12 2016 14:49:04 GMT+0300 (EEST)
 
+
+//before testing a controller make sure you change app.controllerProvider.register to controllers.controller
+//due to the fact that jasmine doesn't know how to register angular controllers using controllerProvider
+
+//Also, manually injecting the bower dependencies is due to the fact that karma loads angular mocks before angular and it get's overwritten,
+//leading to the impossibility to use module and inject in order to create a controller/false services
+
 module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: 'public/',
+    basePath: '',
 
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'requirejs'],
+    frameworks: ['jasmine'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      { pattern: 'components/jquery/dist/jquery.min.js', included: false},
-      { pattern: 'components/angular/angular.js', included: false},
-      { pattern: 'components/angular-resource/angular-resource.js', included: false},
-      { pattern: 'components/oclazyload/dist/ocLazyLoad.js', included: false},
-      { pattern: 'components/angular-route/angular-route.min.js', included: false},
-      { pattern: 'components/angular-ui-router/release/angular-ui-router.min.js', included: false},
-      { pattern: 'components/angular-bootstrap/ui-bootstrap-tpls.min.js', included: false},
-      { pattern: 'components/angular-animate/angular-animate.js', included: false},
-      { pattern: 'javascript/public/app.js', included: false},
-      { pattern: 'javascript/public/controllers.js', included: false},
-      { pattern: 'javascript/public/services.js', included: false},
-      { pattern: 'components/angular-sanitize/angular-sanitize.min.js', included: false},
-      { pattern: 'components/angular-off-click/offClick.js', included: false},
-      { pattern: 'modules/mobile_content_list/mobile_content_list.js', included: false},
-      { pattern: 'modules/sticky-footer/stick_footer.js', included: false},
-      { pattern: 'modules/horizontal-content-list/directive.js', included: false},
-      { pattern: 'modules/widget-most-read/directive.js', included: false},
-      { pattern: 'javascript/shared/controllers.js', included: false},
-      { pattern: 'javascript/shared/directives.js', included: false},
-      { pattern: 'javascript/shared/services.js', included: false},
-      { pattern: 'javascript/public/publicDirectives.js', included: false},
-      { pattern: 'javascript/public/publicFilters.js', included: false},
-      { pattern: 'javascript/public/AuthControllers/AuthModal.js', included: false},
-      { pattern: 'tests/loginStaywelltest.js', included: false, served: true},
-      'test-main.js'
+      { pattern: 'public/components/jquery/dist/jquery.min.js', included: true},
+      { pattern: 'public/components/jquery-ui/jquery-ui.min.js', included: true},
+      { pattern: 'public/components/moment/min/moment.min.js', included: true},
+      { pattern: 'public/components/bootstrap/dist/js/bootstrap.min.js', included: true},
+      { pattern: 'public/components/angular/angular.js', included: true},
+      { pattern: 'public/components/angular-mocks/angular-mocks.js', included: true},
+      { pattern: 'public/components/angular-bootstrap-datetimepicker/src/js/datetimepicker.js', included: true},
+      { pattern: 'public/components/angular-bootstrap-datetimepicker/src/js/datetimepicker.templates.js', included: true},
+      { pattern: 'public/components/qrcode/lib/qrcode.min.js', included: true},
+      { pattern: 'public/components/angular-qr/angular-qr.min.js', included: true},
+      { pattern: 'public/components/angular-sanitize/angular-sanitize.min.js', included: true},
+      { pattern: 'public/components/angular-resource/angular-resource.min.js', included: true},
+      { pattern: 'public/components/angular-route/angular-route.min.js', included: true},
+      { pattern: 'public/components/angular-ui-router/release/angular-ui-router.min.js', included: true},
+      { pattern: 'public/components/angular-bootstrap/ui-bootstrap-tpls.min.js', included: true},
+      { pattern: 'public/modules/angular-select-autocomplete/directive.js', included: true},
+      { pattern: 'public/components/aws-sdk/dist/aws-sdk.min.js', included: true},
+      { pattern: 'public/components/tinymce/tinymce.min.js', included: true},
+      { pattern: 'public/components/ui-select/dist/select.js', included: true},
+      { pattern: 'public/components/tinymce/jquery.tinymce.min.js', included: true},
+      { pattern: 'public/components/ng-file-upload/ng-file-upload.min.js', included: true},
+      { pattern: 'public/components/async/lib/async.js', included: true},
+      { pattern: 'public/components/angular-ui-tree/dist/angular-ui-tree.js"', included: true},
+      { pattern: 'public/components/ng-table/dist/ng-table.js', included: true},
+      { pattern: 'public/modules/ui-tinymce/tinymce.js', included: true},
+      { pattern: 'public/modules/multiple_select/multiple_select.js', included: true},
+      { pattern: 'public/modules/therapeutic_select/therapeutic_select.js', included: true},
+      { pattern: 'public/modules/s3_upload_manager/s3_upload_manager.js', included: true},
+      { pattern: 'public/modules/enhacedS3Upload/enhancedS3Upload.js', included: true},
+      { pattern: 'public/modules/table-action-buttons/action_buttons.js', included: true},
+      { pattern: 'public/modules/admin-emails-list/directive.js', included: true},
+      { pattern: 'public/components/angular-animate/angular-animate.js', included: true},
+      { pattern: 'public/modules/admin-widget-stats/directive.js', included: true},
+      { pattern: 'public/components/ace-builds/src-min-noconflict/ace.js', included: true},
+      { pattern: 'public/components/angular-ui-ace/ui-ace.js', included: true},
+      { pattern: 'public/components/ng-csv/build/ng-csv.min.js', included: true},
+      { pattern: 'public/components/v-accordion/dist/v-accordion.js', included: true},
+      { pattern: 'public/components/angular-ui-tree/dist/angular-ui-tree.js', included: true},
+      { pattern: 'public/modules/bulk_operations/bulk_operations.js', included: true},
+      { pattern: 'public/components/angular-off-click/offClick.js', included: true},
+      { pattern: 'public/components/oclazyload/dist/ocLazyLoad.js', included: true},
+      { pattern: 'public/modules/mobile_content_list/mobile_content_list.js', included: true},
+      { pattern: 'public/modules/sticky-footer/stick_footer.js', included: true},
+      { pattern: 'public/modules/horizontal-content-list/directive.js', included: true},
+      { pattern: 'public/modules/widget-most-read/directive.js', included: true},
+      { pattern: 'public/javascript/public/app.js', included: true},
+      { pattern: 'public/javascript/public/controllers.js', included: true},
+      { pattern: 'public/javascript/public/services.js', included: true},
+      { pattern: 'public/javascript/shared/controllers.js', included: true},
+      { pattern: 'public/javascript/shared/directives.js', included: true},
+      { pattern: 'public/javascript/shared/services.js', included: true},
+      { pattern: 'public/javascript/public/publicDirectives.js', included: true},
+      { pattern: 'public/javascript/public/publicFilters.js', included: true},
+      { pattern: 'public/javascript/public/AuthControllers/AuthModal.js', included: true},
+      { pattern: 'public/tests/loginStaywelltest.js', included: true}
     ],
-    //
+
     // wiredep: {
     //       dependencies: true,    // default: true
-    //       devDependencies: true // default: false
+    //       devDependencies: true, // default: false,
+    //       exclude: [ 'public/components/angular-qr/lib/qrcode.js',
+    //         'public/components/qrcode/lib/qrcode.js',
+    //         'public/components/es5-shim/es5-shim.js',
+    //         'public/components/moment/moment.js',
+    //         'public/components/fullcalendar/dist/fullcalendar.js',
+    //         'public/components/SHA-1/sha1.js',
+    //         'public/components/async/dist/async.js',
+    //         'public/components/angular-ui-ace/ui-ace.js',
+    //           'public/components/qtip2/jquery.qtip.js'
+    //       ]
     // },
 
-
+    // plugins : [ 'karma-chrome-launcher', 'karma-mocha-reporter', 'karma-wiredep' ],
 
       // list of files to exclude
     exclude: [
@@ -65,7 +112,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'mocha'],
 
 
     // web server port
--- public/tests/loginStaywelltest.js
+++ public/tests/loginStaywelltest.js
@@ -1,48 +1,107 @@
 /**
  * Created by andreimirica on 12.04.2016.
  */
-/**
- * Created by andreimirica on 07.04.2016.
- */
+describe('Login Tests', function(){
+    var scope, createController, httpBackend, http, modalInstance, window;
 
-define(['jquery', 'angular', 'ocLazyLoad', 'App', 'controllers', 'services', 'directives', 'filters', 'sharedControllers', 'sharedDirectives', 'sharedServices'], function(jquery, angular, ocLazyLoad, App, controllers, services, directives, filters, sharedControllers, sharedDirectives, sharedServices) {
-    var scope, createController, httpBackend, http, modalInstance, controllerProvider;
-    console.log(ocLazyLoad);
+    beforeEach(function(){
+        module("app");
+        module(function($provide) {
+            // Fake StoreService Implementation returning a promise
 
-    // beforeEach(function() {
-    //     module('app');
-    // });
+            $provide.value('AuthService', {
+                isUserAllowedLogin: function () {
+                    return true
+                },
+                login : {
+                    query: function() {
+                        return {
+                            $promise: {
+                                then :function(callback) {return callback({success: {accepted: true}});},
+                                catch : function (callback) {
+                                    {return callback({success: {accepted: true}});}
+                                }
+                            }
+                        };
+                    }
+                },
+                reset : {
+                    query: function() {
+                        return {
+                            $promise: {
+                                then :function(callback) {return callback({success: {mailto: 'demo@demo.com'}});},
+                                catch : function (callback) {
+                                    {return callback({success: {accepted: true}});}
+                                }
+                            }
+                        };
+                    }
+                },
+                getProHref : function () {
+                    return "pro#";
+                }
+            });
 
-    // beforeEach(inject(function($controller, $rootScope, $httpBackend, $http, $controllerProvider){
-    //     scope = $rootScope.$new();
-    //     httpBackend = $httpBackend;
-    //     console.log($controllerProvider);
-    //     controllerProvider = $controllerProvider;
-    //     http = $http;
-    //     modalInstance = {                    // Create a mock object using spies
-    //         close: jasmine.createSpy('modalInstance.close'),
-    //         dismiss: jasmine.createSpy('modalInstance.dismiss'),
-    //         result: {
-    //             then: jasmine.createSpy('modalInstance.result.then')
-    //         }
-    //     };
-    //     controllerProvider.register('AuthModal', AuthModal);
-    //     createController = function(){
-    //         return $controller('AuthModal', {
-    //             $scope: scope,
-    //             $http : http,
-    //             $modalInstance: modalInstance,
-    //             $controllerProvider: controllerProvider
-    //         });
-    //     }
-    // }));
-describe('Login Test', function(){
-    it('should return 4', function(){
-        // httpBackend.whenGET('partials/public/home.html').respond(200, '');
-        // httpBackend.whenGET('partials/public/auth/login.html').respond(200, '');
-        //httpBackend.flush();
-        // var controller = createController();
-        expect(typeof 222).toBe(typeof 23);
-    })
-    })
+            return null;
+        });
+    });
+
+    beforeEach(inject(function($controller, $rootScope, $httpBackend, $http, $window){
+        scope = $rootScope.$new();
+        httpBackend = $httpBackend;
+        http = $http;
+        window = $window;
+        modalInstance = {                    // Create a mock object using spies
+            close: jasmine.createSpy('modalInstance.close'),
+            dismiss: jasmine.createSpy('modalInstance.dismiss'),
+            result: {
+                then: jasmine.createSpy('modalInstance.result.then')
+            }
+        };
+        createController = function(){
+            return $controller('AuthModal', {
+                $scope: scope,
+                $http : http,
+                $modalInstance: modalInstance,
+                intent: 'login',
+                $window : window
+            });
+        }
+    }));
+
+    it('should return true if login was OK', function(){
+        httpBackend.whenGET('partials/public/home.html').respond(200, '');
+        httpBackend.whenGET('partials/public/auth/login.html').respond(200, '');
+        var controller = createController();
+        scope.login();
+        expect(scope.madeIt).toEqual(true);
+    });
+
+    it('should return false because email address is invalid', function(){
+        httpBackend.whenGET('partials/public/home.html').respond(200, '');
+        httpBackend.whenGET('partials/public/auth/login.html').respond(200, '');
+        var controller = createController();
+        scope.thiz.email = null;
+        scope.thiz.password = 'demo';
+        scope.login();
+        expect(scope.alert.show).not.toEqual(false);
+    });
+
+    it('should return true because we did not complete the email for reset', function() {
+        httpBackend.whenGET('partials/public/home.html').respond(200, '');
+        httpBackend.whenGET('partials/public/auth/login.html').respond(200, '');
+        var controller = createController();
+        scope.thiz.email = null;
+        scope.reset();
+        expect(scope.alert.show).not.toEqual(false);
+    });
+
+    it('should return success because we completed well the email for reset', function() {
+        httpBackend.whenGET('partials/public/home.html').respond(200, '');
+        httpBackend.whenGET('partials/public/auth/login.html').respond(200, '');
+        var controller = createController();
+        scope.thiz.email = 'demo@email.com';
+        scope.reset();
+        expect(scope.alert.type).toEqual('success');
+    });
 });
