ce257d8561c13466aec5b918089e0237b5f94558
andrei.mirica <andrei.mirica@qualitance.com>
2016-07-26 17:50:18 +0300

    MS-1001

--- public/javascript/admin/ProductPageControllers/selectProductType.js
+++ public/javascript/admin/ProductPageControllers/selectProductType.js
@@ -3,7 +3,10 @@
  */
 controllers.controller('SelectProductType', ['$scope', 'SpecialProductsService', '$state', '$modalInstance', function($scope, SpecialProductsService, $state, $modalInstance) {
     $scope.productTypeDisplayNames = ["product", "resource"];
-
+    $scope.alert = {
+        show: false,
+        text: "Va rugam sa selectati un tip!"
+    };
     $scope.productTypeDisplayName = function (name) {
         switch (name) {
             case "product":
@@ -16,12 +19,16 @@ controllers.controller('SelectProductType', ['$scope', 'SpecialProductsService',
     };
 
     $scope.saveSpecialProduct = function () {
-        SpecialProductsService.products.create({productType: $scope.selectedPageType}).$promise.then(function (resp) {
-            $modalInstance.close();
-            $state.reload();
-        }).catch(function (err) {
-            console.log(Error.getMessage(err));
-        });
+        if(!$scope.selectedProductType){
+            $scope.alert.show = true;
+        } else {
+            SpecialProductsService.products.create({productType: $scope.selectedProductType}).$promise.then(function (resp) {
+                $modalInstance.close();
+                $state.reload();
+            }).catch(function (err) {
+                console.log(Error.getMessage(err));
+            });
+        }
     };
 
     $scope.closeModal = function () {
