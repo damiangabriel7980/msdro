d435bd69e223d22af8f05cffc600f0903f4da953
andrei.mirica <andrei.mirica@qualitance.com>
2016-09-29 12:27:28 +0300

    API my prescription

--- app/apiMyPrescription.js
+++ app/apiMyPrescription.js
@@ -13,7 +13,7 @@ module.exports = function (app, logger, router) {
   var handleSuccess = require('./modules/responseHandler/success.js')(logger);
   var handleError = require('./modules/responseHandler/error.js')(logger);
 
-  app.all('/apiAplicationUpgrade', function (req, res, next) {
+  app.all('/apiMyPrescription/*', function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Last-Modified");
@@ -33,6 +33,39 @@ module.exports = function (app, logger, router) {
 
   var PathToAmazon = Config.amazonPrefix + Config.amazonBucket + "/";
 
+  /**
+   * @apiName Get_Prescriptions
+   * @apiDescription Retrieve a list of prescriptions
+   * @apiGroup MyPrescription
+   * @api {get} /apiMyPrescription/config Retrieve a list of prescriptions
+   * @apiVersion 1.0.0
+   * @apiPermission medic
+   * @apiUse HeaderAuthorization
+   * @apiHeader {String} last-modified a date used for filtering prescriptions
+   * @apiExample {curl} Example usage:
+   *     curl -i -H "Authorization: Bearer " -H "last-modified : someDate" http://localhost:8080/apiMyPrescription/config
+   * @apiSuccess {Array} arr an array of prescriptions (or an empty object if the last-modified header is not sent
+   * @apiSuccessExample {json} Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *        [
+   *
+   *        ]
+   *     }
+   * @apiSuccessExample {json} Success-Response (304):
+   *     HTTP/1.1 304 Not Modified
+   *     {
+   *        [
+   *
+   *        ]
+   *     }
+   * @apiUse ErrorOnServer
+   * @apiErrorExample {json} Error-Response (500):
+   *     HTTP/1.1 500 Server Error
+   *     {
+   *
+   *     }
+   */
   router.route('/config')
     .get(function (req, res) {
       if (!req.headers['last-modified']) {
@@ -51,6 +84,38 @@ module.exports = function (app, logger, router) {
 
     });
 
+  /**
+   * @apiName Get_Product_By_Code
+   * @apiDescription Retrieve a product from medic section by a code
+   * @apiGroup MyPrescription
+   * @api {get} /apiMyPrescription/drug Retrieve a product from medic section by a code
+   * @apiVersion 1.0.0
+   * @apiPermission medic
+   * @apiUse HeaderAuthorization
+   * @apiParam {String} drugCode the code of the product
+   * @apiExample {curl} Example usage:
+   *     curl -i -H "Authorization: Bearer " http://localhost:8080/apiMyPrescription/drug?drugCode=windw28312c87dw1
+   * @apiSuccess {Object} obj an object containing the product
+   * @apiSuccessExample {json} Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *        [
+   *
+   *        ]
+   *     }
+   * @apiUse ErrorOnServer
+   * @apiErrorExample {json} Error-Response (500):
+   *     HTTP/1.1 500 Server Error
+   *     {
+   *
+   *     }
+   * @apiUse EntityNotFound
+   * @apiErrorExample {json} Error-Response (4xx):
+   *     HTTP/1.1 404 EntityNotFound Error
+   *     {
+   *
+   *     }
+   */
   router.route('/drug')
     .get(function (req, res) {
       if (req.query.drugCode) {
@@ -87,6 +152,38 @@ module.exports = function (app, logger, router) {
       }
     });
 
+  /**
+   * @apiName Get_Product_By_Id
+   * @apiDescription Retrieve a product from medic section by id
+   * @apiGroup MyPrescription
+   * @api {get} /apiMyPrescription/drug Retrieve a product from medic section by a code
+   * @apiVersion 1.0.0
+   * @apiPermission medic
+   * @apiUse HeaderAuthorization
+   * @apiParam {String} id the id of the product
+   * @apiExample {curl} Example usage:
+   *     curl -i -H "Authorization: Bearer " http://localhost:8080/apiMyPrescription/drug/id?id=windw28312c87dw1
+   * @apiSuccess {Object} obj an object containing the product
+   * @apiSuccessExample {json} Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *        [
+   *
+   *        ]
+   *     }
+   * @apiUse ErrorOnServer
+   * @apiErrorExample {json} Error-Response (500):
+   *     HTTP/1.1 500 Server Error
+   *     {
+   *
+   *     }
+   * @apiUse EntityNotFound
+   * @apiErrorExample {json} Error-Response (4xx):
+   *     HTTP/1.1 404 EntityNotFound Error
+   *     {
+   *
+   *     }
+   */
   router.route('/drug/id')
     .get(function (req, res) {
       if (req.query.id) {
