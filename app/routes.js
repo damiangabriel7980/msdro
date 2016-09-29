e28868d5304a830096a1146f9ded32f300ec5e3a
andrei.mirica <andrei.mirica@qualitance.com>
2016-09-29 16:51:58 +0300

    Main routes API

--- app/routes.js
+++ app/routes.js
@@ -41,6 +41,28 @@ module.exports = function(app, logger, passport) {
 
 // normal routes ===============================================================
 
+    /**
+     * @apiName Redirect_User_To_Proper_Main_Page
+     * @apiDescription Redirect the user to the proper main page
+     * @apiGroup Main_Routes
+     * @api {get} / Redirect the user to the proper main page
+     * @apiVersion 1.0.0
+     * @apiPermission none
+     * @apiExample {curl} Example usage:
+     *     curl http://localhost:8080/
+     * @apiSuccess {html} response the main page
+     * @apiSuccessExample {html} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *
+     *     }
+     * @apiUse ErrorOnServer
+     * @apiErrorExample {json} Error-Response (500):
+     *     HTTP/1.1 500 Server Error
+     *     {
+     *
+     *     }
+     */
     app.get('/', function (req, res) {
         var requestedPRO= 0, requestedActivation = 0, accountActivated = 0, showLogin = 0, accessRoute="", GA_code = my_config.GA_code;
         if(req.query._escaped_fragment_){
@@ -83,6 +105,28 @@ module.exports = function(app, logger, passport) {
         }
     });
 
+    /**
+     * @apiName Redirect_User_To_Medic_Section_If_Authenticated
+     * @apiDescription Redirect the user to the medic section if he's authenticated, otherwise show him the login page/modal window
+     * @apiGroup Main_Routes
+     * @api {get} /pro Redirect the user to the medic section if he's authenticated, otherwise show him the login page/modal window
+     * @apiVersion 1.0.0
+     * @apiPermission none
+     * @apiExample {curl} Example usage:
+     *     curl http://localhost:8080/pro
+     * @apiSuccess {html} response the login page/the main page of public section with the login modal opened
+     * @apiSuccessExample {html} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *
+     *     }
+     * @apiUse ErrorOnServer
+     * @apiErrorExample {json} Error-Response (500):
+     *     HTTP/1.1 500 Server Error
+     *     {
+     *
+     *     }
+     */
 	// show the home page (will also have our login links)
 	app.get('/pro', function(req, res) {
         if(req.isAuthenticated()){
@@ -95,6 +139,28 @@ module.exports = function(app, logger, passport) {
         }
 	});
 
+    /**
+     * @apiName Redirect_User_To_Preview_Section
+     * @apiDescription Redirect the user to preview section of a medic content. If authenticated, redirect him to the content
+     * @apiGroup Main_Routes
+     * @api {get} /preview Redirect the user to preview section of a medic content. If authenticated, redirect him to the content
+     * @apiVersion 1.0.0
+     * @apiPermission none
+     * @apiExample {curl} Example usage:
+     *     curl http://localhost:8080/preview
+     * @apiSuccess {html} response the main page of the preview section
+     * @apiSuccessExample {html} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *
+     *     }
+     * @apiUse ErrorOnServer
+     * @apiErrorExample {json} Error-Response (500):
+     *     HTTP/1.1 500 Server Error
+     *     {
+     *
+     *     }
+     */
     app.get('/preview', function (req, res) {
         if(req.isAuthenticated()) {
             res.redirect('/pro');
@@ -103,6 +169,29 @@ module.exports = function(app, logger, passport) {
         }
     });
 
+    /**
+     * @apiName Log_Out_User
+     * @apiDescription User log out and redirect to public section/login page
+     * @apiGroup Main_Routes
+     * @api {get} /logout User log out and redirect to public section/login page
+     * @apiVersion 1.0.0
+     * @apiPermission medic
+     * @apiPermission admin
+     * @apiExample {curl} Example usage:
+     *     curl http://localhost:8080/logout
+     * @apiSuccess {html} response the main page of the preview section
+     * @apiSuccessExample {html} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *
+     *     }
+     * @apiUse ErrorOnServer
+     * @apiErrorExample {json} Error-Response (500):
+     *     HTTP/1.1 500 Server Error
+     *     {
+     *
+     *     }
+     */
 	// LOGOUT ==============================
 	app.get('/logout', function(req, res) {
         //delete presentations viewed status from current session
@@ -115,6 +204,29 @@ module.exports = function(app, logger, passport) {
 
     // RESET PASSWORD =================================================================================================
 
+    /**
+     * @apiName Reset_Password_Page
+     * @apiDescription Reset a user's password - send forgotPass page
+     * @apiGroup Main_Routes
+     * @api {get} /reset/:token Reset a user's password - send forgotPass page
+     * @apiVersion 1.0.0
+     * @apiPermission none
+     * @apiParam {String} token an reset token
+     * @apiExample {curl} Example usage:
+     *     curl -i http://localhost:8080/reset/iojwdj818281631361hdwwd
+     * @apiSuccess {html} response a HTML page
+     * @apiSuccessExample {HTML} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *
+     *     }
+     * @apiError (Error 4xx) {html} HTMLErrorMessage
+     * @apiErrorExample {html} Error-Response (4xx):
+     *     HTTP/1.1 4xx Error
+     *     {
+     *
+     *     }
+     */
     //enter new password
     // ! used by multiple apps
     app.get('/reset/:token', function(req, res) {
@@ -127,6 +239,31 @@ module.exports = function(app, logger, passport) {
         });
     });
 
+    /**
+     * @apiName Reset_Password_Success
+     * @apiDescription Reset a user's password - change password and return success page
+     * @apiGroup Main_Routes
+     * @api {post} /reset/:token Reset a user's password - change password and return success page
+     * @apiVersion 1.0.0
+     * @apiPermission none
+     * @apiParam {String} token an reset token
+     * @apiParam {String} password the current password
+     * @apiParam {String} confirm the confirmed current password
+     * @apiExample {curl} Example usage:
+     *     curl -i -X POST -d '{"password" : "pass", "confirm" : "pass"}' http://localhost:8080/reset/iojwdj818281631361hdwwd
+     * @apiSuccess {html} response a HTML page
+     * @apiSuccessExample {HTML} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *
+     *     }
+     * @apiError (Error 4xx) {html} HTMLErrorMessage
+     * @apiErrorExample {html} Error-Response (4xx):
+     *     HTTP/1.1 4xx Error
+     *     {
+     *
+     *     }
+     */
     //change user password
     // ! used by multiple apps
     app.post('/reset/:token', function(req, res) {
@@ -187,6 +324,29 @@ module.exports = function(app, logger, passport) {
         }
     });
 
+    /**
+     * @apiName Unsubscribe_Newsletter
+     * @apiDescription Un-subscribe a user from newsletter
+     * @apiGroup Main_Routes
+     * @api {get} /unsubscribe Reset a user's password - change password and return success page
+     * @apiVersion 1.0.0
+     * @apiPermission none
+     * @apiParam {String} hashedEmail a hashed email
+     * @apiExample {curl} Example usage:
+     *     curl -i http://localhost:8080/unsubscribe?user=iojwdj818281631361hdwwd
+     * @apiSuccess {html} response a HTML page
+     * @apiSuccessExample {HTML} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *
+     *     }
+     * @apiError (Error 4xx) {html} HTMLErrorMessage
+     * @apiErrorExample {html} Error-Response (4xx):
+     *     HTTP/1.1 4xx Error
+     *     {
+     *
+     *     }
+     */
     //unusubscribe from newsletter
     app.get('/unsubscribe', function (req, res) {
         if(req.query.user){
@@ -222,6 +382,22 @@ module.exports = function(app, logger, passport) {
     });
 
     //====================================================================================================== access merck manual
+    /**
+     * @apiName Merck_Manual
+     * @apiDescription View the Merck Manual
+     * @apiGroup Main_Routes
+     * @api {get} /merckManual View the Merck Manual
+     * @apiVersion 1.0.0
+     * @apiPermission medic
+     * @apiExample {curl} Example usage:
+     *     curl -i http://localhost:8080/merckManual
+     * @apiSuccess {PDF} response a PDF document
+     * @apiSuccessExample {PDF} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *
+     *     }
+     */
     app.get('/merckManual', isLoggedIn, function (req, res) {
 //        console.log("======================================================================");
 //        var range = req.headers.range;
@@ -241,10 +417,42 @@ module.exports = function(app, logger, passport) {
         res.sendFile('/private_storage/merck.pdf', {root: __dirname});
     });
 
+    /**
+     * @apiName Site_Map
+     * @apiDescription View the Site_Map
+     * @apiGroup Main_Routes
+     * @api {get} /sitemap View the Site_Map
+     * @apiVersion 1.0.0
+     * @apiPermission medic
+     * @apiExample {curl} Example usage:
+     *     curl -i http://localhost:8080/sitemap
+     * @apiSuccess {XML} response a XML document
+     * @apiSuccessExample {XML} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *
+     *     }
+     */
     app.get('/sitemap', function (req, res) {
         res.sendFile('/private_storage/sitemap.xml', {root: __dirname});
     });
 
+    /**
+     * @apiName Skill_share
+     * @apiDescription View the skillshare page
+     * @apiGroup Main_Routes
+     * @api {get} /skillshare View the skillshare page
+     * @apiVersion 1.0.0
+     * @apiPermission medic
+     * @apiExample {curl} Example usage:
+     *     curl -i http://localhost:8080/skillshare
+     * @apiSuccess {html} response a HTML page
+     * @apiSuccessExample {HTML} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *
+     *     }
+     */
     app.get('/skillshare', function (req, res) {
         res.sendFile('/private_storage/skillshare.html', {root: __dirname});
     });
@@ -256,6 +464,36 @@ module.exports = function(app, logger, passport) {
 	// locally --------------------------------
 		// LOGIN =============================== passport login - used by staywell core
 		// process the login form
+    /**
+     * @apiName Login
+     * @apiDescription User login
+     * @apiGroup Main_Routes
+     * @api {post} /login User login
+     * @apiVersion 1.0.0
+     * @apiPermission none
+     * @apiParam {String} username an email
+     * @apiParam {String} password a password
+     * @apiExample {curl} Example usage:
+     *     curl -X POST -i -d '{"username": "josh@test.com", "password" : "pass"}' http://localhost:8080/login
+     * @apiSuccess {html} response a HTML page
+     * @apiSuccessExample {HTML} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *        accepted: true
+     *     }
+     * @apiUse AccessForbidden
+     * @apiErrorExample {json} Error-Response (4xx):
+     *     HTTP/1.1 4xx AccessForbidden Error
+     *     {
+     *
+     *     }
+     * @apiUse BadRequest
+     * @apiErrorExample {json} Error-Response (4xx):
+     *     HTTP/1.1 4xx BadRequest Error
+     *     {
+     *
+     *     }
+     */
 		app.post('/login', function (req, res, next) {
             //middleware to allow flashing messages on empty user/password fields
             if(!req.body.email || !req.body.password){
@@ -294,7 +532,23 @@ module.exports = function(app, logger, passport) {
                 })(req, res, next);
             }
         });
-    
+
+    /**
+     * @apiName Login
+     * @apiDescription User login redirect
+     * @apiGroup Main_Routes
+     * @api {get} /login User login
+     * @apiVersion 1.0.0
+     * @apiPermission none
+     * @apiExample {curl} Example usage:
+     *     curl -i http://localhost:8080/login
+     * @apiSuccess {html} response a HTML page
+     * @apiSuccessExample {HTML} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *        accepted: true
+     *     }
+     */
     //redirect user to home page and open login
     app.get('/login', function (req, res) {
         req.session.showLogin = true;
@@ -303,12 +557,44 @@ module.exports = function(app, logger, passport) {
 
     //activate user account
     // ! used by mobile apps
+    /**
+     * @apiName Activate_Account_Mobile
+     * @apiDescription Activate user account (mobile only)
+     * @apiGroup Main_Routes
+     * @api {get} /activateAccount/:token Activate user account (mobile only)
+     * @apiVersion 1.0.0
+     * @apiPermission none
+     * @apiExample {curl} Example usage:
+     *     curl -i http://localhost:8080/activateAccount/idwi9772313nd7ww
+     * @apiSuccess {html} response a HTML page
+     * @apiSuccessExample {HTML} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *        accepted: true
+     *     }
+     */
     app.get('/activateAccount/:token', function(req, res) {
         res.redirect(307, '/apiMobileShared/activateAccount/'+req.params.token);
     });
 
     //activate user account
     // ! used by Staywell Core
+    /**
+     * @apiName Activate_Account_Web
+     * @apiDescription Activate user account (web only)
+     * @apiGroup Main_Routes
+     * @api {get} /activateAccountStaywell/:token Activate user account (web only)
+     * @apiVersion 1.0.0
+     * @apiPermission none
+     * @apiExample {curl} Example usage:
+     *     curl -i http://localhost:8080/activateAccountStaywell/idwi9772313nd7ww
+     * @apiSuccess {html} response a HTML page
+     * @apiSuccessExample {HTML} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *        accepted: true
+     *     }
+     */
     app.get('/activateAccountStaywell/:token', function (req, res) {
         //put a requestedActivation variable on this session
         //this is used for marking that an info modal needs
@@ -334,6 +620,33 @@ module.exports = function(app, logger, passport) {
         });
     });
 
+    /**
+     * @apiName Check_Email_Exists
+     * @apiDescription Check if a email address is already used by a user
+     * @apiGroup Main_Routes
+     * @api {post} /checkEmailExists Check if a email address is already used by a user
+     * @apiVersion 1.0.0
+     * @apiPermission none
+     * @apiParam {String} email An email address
+     * @apiExample {curl} Example usage:
+     *     curl -i -X POST -d '{"email" : "john@test.com"}' http://localhost:8080/checkEmailExists
+     * @apiSuccess {json} response.success.exists an object confirming the user's existence (can be true or false)
+     * @apiSuccess {json} response.message A message
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *        success : {
+     *              exists: true
+     *        },
+     *        message : "A message"
+     *     }
+     * @apiUse ErrorOnServer
+     * @apiErrorExample {json} Error-Response (500):
+     *     HTTP/1.1 500 Server Error
+     *     {
+     *
+     *     }
+     */
     //check if there is a user already registered wih email
     app.post('/checkEmailExists', function (req, res) {
         User.findOne({'username': UtilsModule.regexes.emailQuery(req.body.email)}, function (err, user) {
