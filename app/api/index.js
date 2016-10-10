c340a748edf744d9de4413a83ef0cf46f5053f18
andrei.mirica <andrei.mirica@qualitance.com>
2016-10-10 13:59:06 +0300

    Read also section

--- app/api/index.js
+++ app/api/index.js
@@ -163,7 +163,7 @@ var getPathologiesWithItems = function(entityToAssociate, itemQParams, pathQPara
                                 display_name: pathology.display_name,
                                 description: pathology.description,
                                 header_image: pathology.header_image,
-                                specialApps: pathology.specialApps,
+                                specialApps: pathology.specialApps.length ? pathology.specialApps: null,
                                 order_index: pathology.order_index,
                                 video_intro: pathology.video_intro
                             };
