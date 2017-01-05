200e0ff0cb0d114bf9fde9ff7fc4825e089f0284
Paul Suceveanu <paulsuceveanu@mac-mini.ad.qualitance.com>
2017-01-05 18:05:57 +0200

    removed default

--- app/models/specialProduct.js
+++ app/models/specialProduct.js
@@ -36,7 +36,7 @@ var specialProductSchema		= new Schema({
     show_sitemap_prescription: Boolean,
     short_description: String,
     dateCreated: {type: Date, default: Date.now},
-    last_updated: {type: Date, default: Date.now}
+    last_updated: Date
 });
 
 specialProductSchema.plugin(deepPopulate, {
